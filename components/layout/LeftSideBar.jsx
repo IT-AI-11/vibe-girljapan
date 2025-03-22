79e418f0698972f782435e93b09bb5a7ca7d807e
Nextjs <tech.united.it@gmail.com>
2025-03-22 12:19:39 +0200

    sign-in button added to LeftSideBar

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -1,12 +1,12 @@
 
 
 
- "use client";
+"use client";
 
 
 
 
-
+import { ClerkProvider, SignedIn, SignedOut, SignInButton } from '@clerk/nextjs'
 import { UserButton, useUser } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import Image from "next/image";
@@ -19,13 +19,13 @@ import { useEffect, useState } from "react";
 
 
 
-//! to (root)/layout.js    левая сторона сайта
- const LeftSideBar = () => {
-   //const { user, isLoaded } = useUser();
+//* to (root)/layout.js    левая сторона сайта
+const LeftSideBar = () => {
+  //const { user, isLoaded } = useUser();
 
-   const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-   const [userData, setUserData] = useState({});
+  const [userData, setUserData] = useState({});
 
   // const getUser = async () => {
   //   const response = await fetch(`/api/user/${user.id}`);
@@ -50,29 +50,31 @@ import { useEffect, useState } from "react";
 
 
 
-  return ( 
+  return (
 
-  
+
     <div className="h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden 2xl:w-[350px] pr-20 custom-scrollbar">
-       <Link href="/">
-      <Image
-        src="https://tse1.mm.bing.net/th?id=OIP.lfKmO91p_sJaYBSg4BIeTAHaE8&pid=Api" // Direct link to the image
-        alt="logo"
-        width={200}
-        height={200}
-      />
-    </Link>
+      <Link href="/">
+        <Image
+          src="https://tse1.mm.bing.net/th?id=OIP.lfKmO91p_sJaYBSg4BIeTAHaE8&pid=Api" // Direct link to the image
+          alt="logo"
+          width={200}
+          height={200}
+        />
+      </Link>
 
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-2 items-center text-light-1">
           <Link href={`/profile/${userData._id}/posts`}>
-            <Image
+
+            {/* <Image
               src={userData?.profilePhoto} 
               alt="profile photo"
               width={50}
               height={50}
               className="rounded-full"
-            />
+            /> */}
+
           </Link>
           <p className="text-small-bold">
             {userData?.firstName} {userData?.lastName}
@@ -101,17 +103,26 @@ import { useEffect, useState } from "react";
       <hr />
 
       <div className="flex gap-4 items-center">
-        {/* <UserButton appearance={{ baseTheme: dark }} afterSignOutUrl="/sign-in"/>    original  */}
-        <UserButton appearance={{ baseTheme: dark }} />
+        {/* <UserButton appearance={{ baseTheme: dark }} afterSignOutUrl="/sign-in"/>            original  */}
+
+        {/* эти кнопки я сам поставил чтобы sign-in появлялся*/}
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        {/* <UserButton appearance={{ baseTheme: dark }} /> */}
         <p className="text-light-1 text-body-bold">Manage Account</p>
       </div>
     </div>
 
   );
 
- };
+};
 
- export default LeftSideBar;
+export default LeftSideBar;
 
 
 
--- components/layout/Menu.jsx
+++ components/layout/Menu.jsx
@@ -12,7 +12,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 
-//! to LeftSideBar.jsx component
+//* to LeftSideBar.jsx component
 const Menu = () => {
 
   const pathname = usePathname();
